2724c9259fd52ab868acc275e87c82f7c8556f69
zhangtao272 <zhangtao272@jd.com>
2018-12-28 18:36:09 +0800

    update input

--- frontPage/js/input.js
+++ frontPage/js/input.js
@@ -12,64 +12,76 @@ var _input = {
     },
     events: {
         bindEvent: function (opt) {
-            // $(n) 缓存
             $.each(opt.array, function (i, n) {
                 //缓存_obj'
-                $(n).data(_input.options.cacheName, _input.options._obj);
+                var $n = $(n);
+                $n.data(_input.options.cacheName, _input.options._obj);
 
+                // textarea
                 if (opt.selector === opt.options.textareaClassName) {
-                    // todo 原生带文字
-                    $(n).wrap('<div class="' + opt.options.groupClassName + '"></div>');
-                    $(n).after(
+                    $n.wrap('<div class="' + opt.options.groupClassName + '"></div>');
+                    $n.after(
                         '<div class="' + opt.options.txtNumClassName + '">' +
-                        '<span>' + n.value.length + '</span>' +
+                        '<span>' + $n.val().length + '</span>' +
                         '<span>/</span>' +
                         '<span>' + n.maxLength || 100 + '</span>' +
                         '</div>'
                     );
-                    // todo keyup外的触发 鼠标、js赋值、初始化
-                    $(n).off("keyup").on("keyup", _input.eventFn.inputNumFn);
+
+                    // todo BUG：dom插入数据不触发
+                    $n.off("input propertychange").on("input propertychange", _input.eventFn.inputNumFn);
                 } else {
                     // 初始化删除按钮
-                    if ($(n).hasClass(opt.options.clearClassName)) {
-                        $(n).wrap('<div class="' + opt.options.groupClassName + '"></div>');    // add group element
-                        $(n).after('<i class="' + opt.options.iconCloseCircle + '"></i>');  // add clean icon
+                    if ($n.hasClass(opt.options.clearClassName)) {
+                        $n.wrap('<div class="' + opt.options.groupClassName + '"></div>');    // add group element
+                        $n.after('<i class="' + opt.options.iconCloseCircle + '"></i>');  // add clean icon
 
-                        var selscter = $(n).closest(opt.options.groupSelector).children("." + opt.options.iconCloseCircle);
+                        var selscter = $n.closest(opt.options.groupSelector).children("." + opt.options.iconCloseCircle);
                         selscter.data(_input.options.cacheName, _input.options._obj);
 
-                        // todo change
-                        $(n).off("input change").on("input change", _input.eventFn.showIconFn);
-                        if (selscter.length > 0) {
-                            selscter.off("click").on("click", _input.eventFn.clearFn);
+                        // 监听input变化
+                        // todo BUG：dom插入数据不触发
+                        $n.off("input propertychange").on("input propertychange", _input.eventFn.showIconFn);
+                        // 初始化默认值
+                        if ($n.val().length > 0) {
+                            $n.closest(opt.options.groupSelector).children('.' + opt.options.iconCloseCircle).show();
                         }
+
+                        // 监听清理图标点击事件
+                        selscter.off("click").on("click", _input.eventFn.clearFn);
                     }
                 }
             });
         }
     },
     eventFn: {
+        // 点击清理图标
         clearFn: function () {
-            var _obj = $(this).data(_input.options.cacheName);
-            $(this).closest(_obj.options.groupSelector).children(_obj.selector).val("");
-            $(this).hide();
+            var $this = $(this),
+                _obj = $this.data(_input.options.cacheName);
+            $this.closest(_obj.options.groupSelector).children(_obj.selector).val('');
+            $this.hide();
         },
-        showIconFn: function () {
-            console.log(1);
-            var _obj = $(this).data(_input.options.cacheName);
-            var Selector = $(this).closest(_obj.options.groupSelector).children('.' + _obj.options.iconCloseCircle);
 
-            if ($(this).val().length > 0) {
+        // 显示清空图标
+        showIconFn: function () {
+            console.log(111);
+            var $this = $(this),
+                _obj = $this.data(_input.options.cacheName),
+                Selector = $this.closest(_obj.options.groupSelector).children('.' + _obj.options.iconCloseCircle);
+
+            if ($this.val().length > 0) {
                 Selector.show();
             } else {
                 Selector.hide();
             }
         },
+        // 改变字数
         inputNumFn: function () {
-            var _obj = _input.options._obj;
-            var txtLength = $.trim($(this).val()).length;
-            var maxLength = $(this).attr('maxLength');
-            var Selector = $(this).next(_obj.options.txtNumSelector).children('span').eq(0);
+            var _obj = _input.options._obj,
+                txtLength = $.trim($(this).val()).length,
+                maxLength = $(this).attr('maxLength'),
+                Selector = $(this).next(_obj.options.txtNumSelector).children('span').eq(0);
 
             Selector.text(txtLength);
 
